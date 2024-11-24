268f1e44ef0889756e59dde0e52db8f1238ddc90
Coba Paz <cobajayy@gmail.com>
2024-11-24 14:23:03 -0700

    change code

---
+++ functions.js
@@ -0,0 +1,69 @@
+// // // 1.
+
+// // const print = function (message) {
+// //     console.log(message)
+// // }
+
+// // print("hello")
+// // print("what's up")
+
+// // // 2.
+
+
+// // function multiply(num1, num2 = 2){
+// //     const total = num1 * num2
+// //     console.log(total)
+// // }
+
+// // multiply(10, 5)
+// // multiply(10)
+
+// // // 3. 
+
+// // function maxOfTwoNumbers (num1, num2) {
+// //     if (num1 >= num2) {
+// //         console.log(num1)
+// //     } else (
+// //         console.log(num2)
+// //     )
+// // }
+
+// // maxOfTwoNumbers(10, 15)
+
+
+// // // 4. 
+
+// function maxOfThree (num1, num2, num3) {
+//     if (num1 >= num2 && num1 >= num3) {
+//                 console.log(num1)
+//             } 
+//             else if (num2 >= num1 && num2 >= num3){
+//                 console.log(num2)
+//             } else {
+//                 console.log(num3)
+//             }
+//         }
+
+// maxOfThree(10, 2, 16)
+
+// // // 5.
+
+function sumArray(numbers) {
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+       sum = sum + numbers[i]
+
+    }
+
+    console.log(sum)
+}
+
+sumArray([2, 4, 5])
+
+
+// // // 6.
+// // // 7.
+// // // 8.
+// // // 9.
+// // // 10.
